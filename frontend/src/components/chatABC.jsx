f5fa193e3024eefe419c0d3f0d0b056444bda5eb
Sreehari <sreeharimangalasseri528@gmail.com>
2025-01-28 09:16:13 +0530

    Erri=orNotOver

---
+++ frontend/src/components/chatABC.jsx
@@ -0,0 +1,60 @@
+<div className="flex-1 overflow-y-auto p-4 space-y-4">
+  {/* Message 1 */}
+  <div className="chat chat-end">
+    {/* <div className="chat-image avatar">
+      <div className="size-10 rounded-full border">
+        <img
+          src="./authUserAvatar.png"
+          alt="profile pic"
+        />
+      </div>
+    </div> */}
+    <div className="chat-header mb-1">
+      <time className="text-xs opacity-50 ml-1">10:00 AM</time>
+    </div>
+    <div className="chat-bubble flex flex-col">
+      <p>Hello, how are you?</p>
+    </div>
+  </div>
+
+  {/* Message 2 */}
+  <div className="chat chat-start">
+    {/* <div className="chat-image avatar">
+      <div className="size-10 rounded-full border">
+        <img
+          src="./selectedUserAvatar.png"
+          alt="profile pic"
+        />
+      </div>
+    </div> */}
+    <div className="chat-header mb-1">
+      <time className="text-xs opacity-50 ml-1">10:01 AM</time>
+    </div>
+    <div className="chat-bubble flex flex-col">
+      <p>I'm good, thank you! How about you?</p>
+    </div>
+  </div>
+
+  {/* Message 3 with an Image */}
+  <div className="chat chat-end">
+    {/* <div className="chat-image avatar">
+      <div className="size-10 rounded-full border">
+        <img
+          src="./authUserAvatar.png"
+          alt="profile pic"
+        />
+      </div>
+    </div> */}
+    <div className="chat-header mb-1">
+      <time className="text-xs opacity-50 ml-1">10:02 AM</time>
+    </div>
+    <div className="chat-bubble flex flex-col">
+      {/* <img
+        src="./imageAttachment.png"
+        alt="Attachment"
+        className="sm:max-w-[200px] rounded-md mb-2"
+      /> */}
+      <p>Here’s the image you asked for!</p>
+    </div>
+  </div>
+</div>
