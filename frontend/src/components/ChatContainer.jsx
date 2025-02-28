f97f004d5f4741325d6a16e5beec6b462a8b720a
Sreehari <sreeharimangalasseri528@gmail.com>
2025-02-28 16:37:31 +0530

    MessageErrorHandled

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -83,7 +83,7 @@ const ChatContainer = () => {
         ) : (
           <WelcomeChat />
         )
-      ) : (
+      ) : ( 
         <>
           <div className="flex-1 overflow-y-auto p-4 space-y-4">
             <ChatBubbles /> {/* Show chat messages if form is not shown */}
