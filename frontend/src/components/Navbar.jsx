227988b1054fbf731d5a373ea65911285dc1ee65
Sreehari <sreeharimangalasseri528@gmail.com>
2025-03-17 12:33:41 +0530

    UserAuthFixes&NavBarOginChange

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from "react";
-import { LogIn, MessageSquare, Settings } from "lucide-react";
+import { Settings } from "lucide-react";
 import { Link } from "react-router-dom";
+import { useAuthStore } from "../store/useAuthStore";
 
 const Navbar = ({ onOpenTicket }) => {
+  const { userAuth } = useAuthStore();
+
   return (
     <header className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 backdrop-blur-lg bg-base-100/80">
       <div className="container mx-auto px-4 h-16">
@@ -13,7 +16,6 @@ const Navbar = ({ onOpenTicket }) => {
               className="flex items-center gap-2.5 hover:opacity-80 transition-all"
             >
               <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
-                {/* <MessageSquare className="w-5 h-5 text-primary" /> */}
                 <img src="/SmartTeams.jpg" alt="Logo" className="w-15 h-15" />
               </div>
               <h1 className="text-lg font-bold">Smart Teams</h1>
@@ -21,22 +23,29 @@ const Navbar = ({ onOpenTicket }) => {
           </div>
 
           <div className="flex items-center gap-2">
-            <Link to="/settings" className="btn btn-sm gap-2 transition-colors">
-              <Settings className="w-4 h-4" />
-              <span className="hidden sm:inline">Settings</span>
-            </Link>
+            {userAuth && (
+              <>
+                <Link
+                  to="/settings"
+                  className="btn btn-sm gap-2 transition-colors"
+                >
+                  <Settings className="w-4 h-4" />
+                  <span className="hidden sm:inline">Settings</span>
+                </Link>
 
-            <Link to="/login" className="btn btn-sm gap-2 transition-colors">
+                <button
+                  className="btn btn-sm gap-2 transition-colors"
+                  onClick={onOpenTicket}
+                >
+                  <span className="hidden sm:inline">Raise A Ticket</span>
+                </button>
+              </>
+            )}
+
+            {/* <Link to="/login" className="btn btn-sm gap-2 transition-colors">
               <LogIn className="w-4 h-4" />
               <span className="hidden sm:inline">Login Page</span>
-            </Link>
-
-            <button
-              className="btn btn-sm gap-2 transition-colors"
-              onClick={onOpenTicket}
-            >
-              <span className="hidden sm:inline">Raise A Ticket</span>
-            </button>
+            </Link> */}
           </div>
         </div>
       </div>
