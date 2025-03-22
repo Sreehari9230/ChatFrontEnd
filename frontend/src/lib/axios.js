1d948d54e4621ef448f1c3f2344377aec64b74d3
Sreehari <sreeharimangalasseri528@gmail.com>
2025-03-22 15:29:49 +0530

    newBaseUrl

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: "https://v5dmsmd1-8000.inc1.devtunnels.ms/",
+    baseURL: "http://13.61.154.213/",
     withCredentials: true,
 })
