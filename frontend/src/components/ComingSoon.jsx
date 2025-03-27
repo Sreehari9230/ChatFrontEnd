6f3b5f44b19cd8155a13691334e86a8a80db6d7d
Sreehari <sreeharimangalasseri528@gmail.com>
2025-03-27 12:34:22 +0530

    ComingSoonUi&ScrollAnimation

---
+++ frontend/src/components/ComingSoon.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ComingSoon = () => {
+  return (
+    <div className="w-full flex flex-1 flex-col items-center justify-center p-16 bg-base-100/50">
+      <h1 className="text-4xl font-bold text-primary">Coming Soon</h1>
+    </div>
+  );
+};
+
+export default ComingSoon;
