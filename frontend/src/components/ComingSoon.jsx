bee9818af8e43ff127fe0711d46f1d3ccbafda5c
Sreehari <sreeharimangalasseri528@gmail.com>
2025-03-27 12:45:41 +0530

    UiChangedAgain

--- frontend/src/components/ComingSoon.jsx
+++ frontend/src/components/ComingSoon.jsx
@@ -1,17 +1,23 @@
 import React from "react";
+import { Construction } from "lucide-react";
 
 const ComingSoon = () => {
   return (
     <div className="w-full flex flex-1 flex-col items-center justify-center p-16 bg-base-100/50">
-    <div className="flex flex-col items-center space-y-4">
-      <span className="text-6xl text-primary">
-        🚧 
-      </span>
-      <h1 className="text-4xl font-bold text-primary">Coming Soon</h1>
-      <p className="text-lg text-primary">The <span className="font-semibold text-secondary">Software Development Department</span> is on its way!</p>
+      <div className="flex flex-col items-center space-y-4">
+        <span className="text-6xl text-secondary">
+          <Construction size={80} strokeWidth={1.5} />
+        </span>
+        <h1 className="text-4xl font-bold text-primary">Coming Soon</h1>
+        <p className="text-lg text-primary">
+          The{" "}
+          <span className="font-semibold text-secondary">
+            Software Development Department
+          </span>{" "}
+          is on its way!
+        </p>
+      </div>
     </div>
-  </div>
-  
   );
 };
 
