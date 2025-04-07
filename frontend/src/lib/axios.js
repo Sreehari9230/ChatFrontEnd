77be401ddf9646490b96dd320592f4d3dbf592b6
Sreehari <sreeharimangalasseri528@gmail.com>
2025-04-07 17:01:40 +0530

    urlChanged

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: "https://smartteams.nypus.in/",
+    baseURL: "https://v5dmsmd1-8000.inc1.devtunnels.ms",
     withCredentials: true,
 })
 // https://v5dmsmd1-8000.inc1.devtunnels.ms/login
